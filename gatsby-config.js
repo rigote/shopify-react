18983968ca3c362593a098d5b89800ed806a05dc
Rigote <matheus.rigote@samuraiexperts.com.br>
2022-03-22 15:55:29 -0300

    Ajustes projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,8 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`, // This path is relative to the root of the site.
+        display: `minimal-ui`,
+        icon: `${__dirname}/src/images/logo.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
